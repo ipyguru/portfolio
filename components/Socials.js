25980e1b6426b1ef9db0f5eb9e55fa130b7888c4
ipguru <vbuots@icloud.com>
2024-09-11 15:11:31 +0400

    add header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,21 @@
-const Socials = () => {
-  return <div>Socials</div>;
-};
+import Link from 'next/link'
+import { RiGithubLine, RiVkLine, RiYoutubeLine } from 'react-icons/ri'
 
-export default Socials;
+const Socials = () => {
+  return (
+    <div className='flex items-center gap-x-5 text-2xl'>
+      <Link href={''} className='' target='_blank'>
+        <RiYoutubeLine className='hover:text-accent transition-all duration-300' />
+      </Link>
+      <Link href={''} className='' target='_blank'>
+        <RiVkLine className='hover:text-accent transition-all duration-300' />
+      </Link>
+      <Link href={''} className='' target='_blank'>
+        <RiGithubLine className='hover:text-accent transition-all duration-300' />
+      </Link>
+      {/* more */}
+    </div>
+  )
+}
+
+export default Socials
